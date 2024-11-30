c614d5c0253bd79a20880198b362afdecb9785bb
MisterKermit <97013614+MisterKermit@users.noreply.github.com>
2024-11-29 17:10:39 -1000

    Todays changes
    - not working -
    -postgress connection

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,63 +2,29 @@ import express, { Express, Request, Response } from "express";
 import WebSocket from "ws";
 import cors from "cors";
 import { generatePixels, NetPixel, NetPixelMatrix } from "./util";
+import { Client } from "pg";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const client = new Client({
+  user: "postgres",
+  host: "172.18.0.2",
+  // database: "users",
+  password: "example",
+  port: 3002,
+});
+
+async function databaseStuff(client: Client): Promise<string> {
+  // await client.connect();
+
+  console.log("connected!");
+  const result = await client.query("SELECT 1+1 AS result;");
+  return "I got:" + result.rows.toString()
+}
+
 
 const server = new WebSocket.Server({
   port: 3001,
-});
-
-const pixels: NetPixelMatrix = generatePixels();
-
-let sockets: WebSocket[] = [];
-server.on("connection", function(socket) {
-  sockets.push(socket);
-
-  // When you receive a message, send that message to every socket.
-  socket.on("message", function(msg) {
-
-    interface Message {
-      type: string
-    }
-
-    interface PixelChange {
-      type: string,
-      pixelColor: number,
-      xPos: number,
-      yPos: number,
-    }
-
-    interface ChatMessage {
-      type: string,
-      message: string
-    }
-
-    let req = msg.toString();
-    let raw: Message = JSON.parse(req)
-    if (raw.type == "pixelColor") {
-      const pixel = raw as PixelChange;
-      
-      const netPixel: NetPixel = {
-        color: pixel.pixelColor,
-        user: null
-      }
-      pixels[pixel.yPos][pixel.xPos] = netPixel;
-    } else if (raw.type == "chatMessage") {
-      const newMessage = raw as ChatMessage;
-    }
-    
-    
-
-
-    sockets.forEach((s) => s.send(JSON.stringify(raw)));
-  });
-
-  // When a socket closes, or disconnects, remove it from the array.
-  socket.on("close", function() {
-    sockets = sockets.filter((s) => s !== socket);
-  });
 });
 
 app.use(
@@ -67,13 +33,61 @@ app.use(
   })
 );
 
-
-app.get("/", (_req: Request, res: Response) => {
-  res.send("It works");
+app.get("/", async (_req: Request, res: Response) => {
+  const dbRes = await databaseStuff(client);
+  res.send(dbRes)
 });
+
 
 app.get("/pixels", (_req: Request, res: Response) => {
   res.json(pixels);
+});
+
+const pixels: NetPixelMatrix = generatePixels();
+
+let sockets: WebSocket[] = [];
+server.on("connection", function (socket) {
+  sockets.push(socket);
+
+  // When you receive a message, send that message to every socket.
+  socket.on("message", function (msg) {
+    interface Message {
+      type: string;
+    }
+
+    interface PixelChange {
+      type: string;
+      pixelColor: number;
+      xPos: number;
+      yPos: number;
+    }
+
+    interface ChatMessage {
+      type: string;
+      message: string;
+    }
+
+    let req = msg.toString();
+    let raw: Message = JSON.parse(req);
+    if (raw.type == "pixelColor") {
+      const pixel = raw as PixelChange;
+
+      const netPixel: NetPixel = {
+        color: pixel.pixelColor,
+        user: null,
+      };
+      pixels[pixel.yPos][pixel.xPos] = netPixel;
+    } else if (raw.type == "chatMessage") {
+      const newMessage = raw as ChatMessage;
+    }
+
+    sockets.forEach((s) => s.send(JSON.stringify(raw)));
+  });
+
+  // When a socket closes, or disconnects, remove it from the array.
+  socket.on("close", function () {
+    sockets = sockets.filter((s) => s !== socket);
+  });
 });
 
 app.listen(port, () => {
